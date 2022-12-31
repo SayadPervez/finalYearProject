9f0b77d5ccc4d38f8c16650b518f69cf3100a885
Sayad Pervez <pervez2504@gmail.com>
2022-12-31 11:49:49 +0530

    sourceSim semi completed

--- utils/sourceSimulate.py
+++ utils/sourceSimulate.py
@@ -1,5 +1,5 @@
 from konstants import equiTriangleSide as a, speedOfSound as v
-from math import cos,sin,radians,dist
+from math import cos,sin,degrees,radians,dist,atan
 
 def polar2cart(polarPoint):
     return(polarPoint[0]*cos(radians(polarPoint[1])),polarPoint[0]*sin(radians(polarPoint[1])))
@@ -29,13 +29,38 @@ def calculateTime(sourcePoint,theta=0,debug=False):
 
     print(tA,tB,tC)
 
+    sourceAngle = degrees(atan(sourcePoint[1]/sourcePoint[0]))# wrt (0,0)
+    print(sourceAngle)
+
     if(debug):
-        import matplotlib.pyplot as plt
-        myScatter(plt,(0,0),"green","Origin",22)
-        myScatter(plt,cartA,"blue","A")
-        myScatter(plt,cartB,"blue","B")
-        myScatter(plt,cartC,"blue","C")
-        myScatter(plt,sourcePoint,"red","Source")
+        from matplotlib import pyplot as plt, patches
+
+        fig = plt.figure()
+        ax = fig.add_subplot()
+
+        plt.axhline(y=0, color='black', linestyle=':')
+        plt.axvline(x=0, color='black', linestyle=':')
+
+        myScatter(ax,cartA,"blue",f"A ({round(tA*1000,3)} ms)")
+        myScatter(ax,cartB,"blue",f"B ({round(tB*1000,3)} ms)")
+        myScatter(ax,cartC,"blue",f"C ({round(tC*1000,3)} ms)")
+        myScatter(ax,sourcePoint,"red","Source")
+
+        circleA = patches.Circle(sourcePoint,dA,color="red",fill=False)
+        circleB = patches.Circle(sourcePoint,dB,color="red",fill=False)
+        circleC = patches.Circle(sourcePoint,dC,color="red",fill=False)
+
+        ax.add_patch(circleA)
+        ax.add_patch(circleB)
+        ax.add_patch(circleC)
+
+        ax.set_aspect('equal', adjustable='box')
+        ax.spines[['right', 'top']].set_visible(False)
+
+        plt.title("Source Debug Plot")
+        plt.xlabel("X Axis")
+        plt.ylabel("Y Axis")
+
         plt.show()
 
-calculateTime((0.2,0.2),debug=True)
+calculateTime((0.3,3**0.4/10),273,debug=True)
