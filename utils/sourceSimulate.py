47c63a1051742f0a3914176e74a2860919bfa4ba
Sayad Pervez <pervez2504@gmail.com>
2022-12-31 12:41:42 +0530

    sourceSimulate now returns

--- utils/sourceSimulate.py
+++ utils/sourceSimulate.py
@@ -1,5 +1,5 @@
 from konstants import equiTriangleSide as a, speedOfSound as v
-from math import cos,sin,degrees,radians,dist,atan
+from math import cos,sin,degrees,radians,dist,atan,ceil
 
 def polar2cart(polarPoint):
     return(polarPoint[0]*cos(radians(polarPoint[1])),polarPoint[0]*sin(radians(polarPoint[1])))
@@ -27,10 +27,10 @@ def calculateTime(sourcePoint,theta=0,debug=False):
     tB = dB/v
     tC = dC/v
 
-    print(tA,tB,tC)
+    #print(tA,tB,tC)
 
     sourceAngle = degrees(atan(sourcePoint[1]/sourcePoint[0]))# wrt (0,0)
-    print(sourceAngle)
+    #print(sourceAngle)
 
     if(debug):
         from matplotlib import pyplot as plt, patches
@@ -63,4 +63,30 @@ def calculateTime(sourcePoint,theta=0,debug=False):
 
         plt.show()
 
-calculateTime((0.3,3**0.4/10),273,debug=True)
+        myRange = ceil(max([tA,tB,tC])*1000000+20)
+        _X = list(range(myRange))
+        _Y = [("Detected" if ( ceil(_)==ceil(tA*1000000) or ceil(_)==ceil(tB*1000000) or ceil(_)==ceil(tC*1000000) ) else "Not Detected") for _ in _X]
+        
+        plt.step(_X,_Y)
+        plt.text(ceil(tA*1000000),"Detected",f"A\n{round(tA*1000,3)} ms",weight="bold")
+        plt.text(ceil(tB*1000000),"Detected",f"B\n{round(tB*1000,3)} ms",weight="bold")
+        plt.text(ceil(tC*1000000),"Detected",f"C\n{round(tC*1000,3)} ms",weight="bold")
+
+        plt.title("Signal Delay Plot")
+        plt.xlabel("Time (in ms)")
+        plt.ylabel("Specific Frequency Detected")
+        plt.show()
+
+    sortList = sorted([["A",tA],["B",tB],["C",tC]],key=lambda x:x[1])
+    
+    returnObject = {}
+
+    for i,x in enumerate(sortList):
+        returnObject[f"Impact{i+1}"] = {
+            "point" : x[0],
+            "time" : x[1]
+        }
+
+    return(returnObject)
+
+print(calculateTime((0.3,3**0.4/10),250))
