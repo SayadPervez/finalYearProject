ac5a10fbfc8d5e287aa734fdc1071fc86150b421
sayad.pervez <sayad.pervez@zohocorp.com>
2023-02-23 22:27:48 +0530

    main.py initializex

--- hardwareImplementations/main.py
+++ hardwareImplementations/main.py
@@ -1,2 +1,32 @@
-with open("./tty.log","r") as f:
-    raw_data = f.readlines()
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.signal import hamming
+
+def transform(data):
+    window = hamming(len(data))
+    data = data * window
+
+    sample_rate = 10000
+    spectrum = np.fft.fft(data)
+    frequency = np.fft.fftfreq(len(data), 1/sample_rate)
+    spectrum = abs(spectrum[:len(spectrum)//2])
+    frequency = frequency[:len(frequency)//2]
+
+    plt.plot(frequency, spectrum)
+    plt.xlabel('Frequency (Hz)')
+    plt.ylabel('Amplitude')
+    plt.show()
+
+def readFileAsList():
+    with open("./tty.log","r") as f:
+        return f.readlines()
+
+def processData(array):
+    return [int(_.split(" ")[-1]) for _ in array]
+
+def getSubArray(entireData,offset,size):
+    return entireData[offset:offset+size]
+
+entireData = readFileAsList()
+subData = processData(getSubArray(entireData,0,256))
+transform(subData)
