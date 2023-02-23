bf574ea9748227ee16ed7f7894407ae45d2d97ad
sayad.pervez <sayad.pervez@zohocorp.com>
2023-02-23 12:14:48 +0530

    changed according to zoho lap

--- hardwareImplementations/readSerial.py
+++ hardwareImplementations/readSerial.py
@@ -1,7 +1,14 @@
-port = "/dev/tty" + input("Enter PORT PATH ( /dev/tty* ) : ")
-baudrate = int(input("Enter baudrate : "))
-
 import os
 
+os.system("ls /dev/tty*")
+
+port = "/dev/tty" + input("\n\nEnter PORT PATH ( /dev/tty* ) : ")
+baudrate = int(input("Enter baudrate : "))
+
+try:
+    os.remove("./tty.log")
+except Exception as e:
+    print(e)
+
 os.system(f"""sudo stty -F {port} {baudrate}""")
-os.system(f"""sudo stdbuf -o0 cat {port} | ts '[%Y-%m-%d %H:%M:%S]' > /home/zorin/Desktop/tty.log""")
+os.system(f"""sudo stdbuf -o0 cat {port} | ts '[%Y-%m-%d %H:%M:%S]' > ./tty.log""")
