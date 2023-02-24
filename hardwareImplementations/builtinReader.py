0b450aebbc40fd88bf9ab889fdcd7352d07318eb
sayad.pervez <sayad.pervez@zohocorp.com>
2023-02-24 22:20:06 +0530

    stable build

--- hardwareImplementations/builtinReader.py
+++ hardwareImplementations/builtinReader.py
@@ -1,14 +1,22 @@
-import sounddevice as sd
 import numpy as np
+import matplotlib.pyplot as plt
+import sounddevice as sd
 
-timeDomainArray = []
+# Define the sampling rate and duration of the recording
+fs = 44100  # Hz
+duration = 15  # seconds
 
-def print_sound(indata, outdata, frames, time, status):
-    volume_norm = np.linalg.norm(indata)*10
-    timeDomainArray.append("[d t] "+str(int(volume_norm)))
+# Start the recording
+print("Recording...")
+recording = sd.rec(int(fs * duration), samplerate=fs, channels=1)
 
-with sd.Stream(callback=print_sound,samplerate=40000):
-    sd.sleep(10000)
+# Wait for the recording to complete
+sd.wait()
+
+# Extract the audio signal and calculate its Fourier transform
+audio_signal = recording[:, 0]  # Extract the first channel (mono)
+
+audio_signal = ["[d t] "+str(_) for _ in audio_signal]
 
 with open("./tty.log","w") as f:
-    f.write("\n".join(timeDomainArray))
+    f.write("\n".join(audio_signal))
--- hardwareImplementations/main.py
+++ hardwareImplementations/main.py
@@ -5,20 +5,20 @@ from scipy.signal import hamming
 skipEvery = 500
 skipAmount = 5000
 
-def transform(data,sample_rate = 10000,threshold=1000):
+def transform(data,sample_rate = 10000,threshold=100):
     window = hamming(len(data))
     data = data * window
 
     spectrum = np.fft.fft(data)
     frequency = np.fft.fftfreq(len(data), 1/sample_rate)
-    spectrum = abs(spectrum[:len(spectrum)//2])[10:]
-    frequency = frequency[:len(frequency)//2][10:]
+    spectrum = abs(spectrum[:len(spectrum)//2])[10:-10]
+    frequency = frequency[:len(frequency)//2][10:-10]
 
     sR = [_ for _ in spectrum if(_>threshold)]
     fR = [frequency[_] for _ in range(len(spectrum)) if(spectrum[_]>threshold)]
 
     plt.plot(frequency, spectrum)
-    plt.plot(fR, sR, color="r")
+    plt.scatter(fR, sR, color="r")
     plt.xlabel('Frequency (Hz)')
     plt.ylabel('Amplitude')
     plt.pause(0.001)
@@ -29,14 +29,14 @@ def readFileAsList():
         return f.readlines()
 
 def processData(array):
-    return [int(_.split(" ")[-1]) for _ in array]
+    return [float(_.split(" ")[-1])*100 for _ in array]
 
 def getSubArray(entireData,offset,size):
     return entireData[offset:offset+size]
 
 entireData = readFileAsList()
 
-offset = 1
+offset = 230000
 skipAmount = 1
 while(offset<len(entireData)-256):
 
@@ -44,8 +44,8 @@ while(offset<len(entireData)-256):
     print(offset)
     if(offset%skipEvery==0):
         offset+=skipAmount
-    transform(subData,40000)
+    transform(subData,44100)
 
-    offset+=1
+    offset+=50
 
 plt.show()
