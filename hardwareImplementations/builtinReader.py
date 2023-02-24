cf424d260fa99546a6e845c16193f64b5c5f1b2f
sayad.pervez <sayad.pervez@zohocorp.com>
2023-02-24 19:25:57 +0530

    builtin reader adapted to standards

--- hardwareImplementations/builtinReader.py
+++ hardwareImplementations/builtinReader.py
@@ -5,10 +5,10 @@ timeDomainArray = []
 
 def print_sound(indata, outdata, frames, time, status):
     volume_norm = np.linalg.norm(indata)*10
-    timeDomainArray.append(str(volume_norm))
+    timeDomainArray.append("[d t] "+str(int(volume_norm)))
 
 with sd.Stream(callback=print_sound,samplerate=10000):
-    sd.sleep(2000)
+    sd.sleep(10000)
 
 with open("./tty.log","w") as f:
     f.write("\n".join(timeDomainArray))
