9004d0734cb393e28a8ccbdf87a1c8c85370a976
sayad.pervez <sayad.pervez@zohocorp.com>
2023-03-02 17:04:10 +0530

    CAB completed

---
+++ docgen/CAB.py
@@ -0,0 +1,66 @@
+from matplotlib import pyplot as plt
+from formulae import getVertices,drawLine,drawAngle
+
+allVertices = getVertices()
+xLi = [_[0] for _ in allVertices]
+yLi = [_[1] for _ in allVertices]
+
+def drawBaseStructure():
+    fig, ax = plt.subplots()
+
+    plt.axhline(y=0, color='black', linestyle=':')
+    plt.axvline(x=0, color='black', linestyle=':')
+
+    plt.plot(xLi+[xLi[0]],yLi+[yLi[0]],c="b",label="Equilateral Triangular Structure")
+    plt.scatter(xLi,yLi,c="k",label="Vertices of Triangle")
+
+    plt.legend()
+
+    ax.set_aspect('equal', adjustable='box')
+    ax.spines[['right', 'top']].set_visible(False)
+
+    # wave approaching at 30 degrees to the X axis
+    # (1,0) & (0,2.732)
+    return fig,ax
+
+hitOrder="ABC"
+
+waveAngle = 140 # degrees
+lineAngle = waveAngle+90
+
+fig,ax = drawBaseStructure()
+drawLine(ax,lineAngle,(-1,0),"sound wave")
+ax.set_title(f'Wave at {waveAngle} Degrees')
+plt.xlabel("X Axis Distance in meters (m)")
+plt.ylabel("Y Axis Distance in meters (m)")
+plt.legend()
+plt.show()
+
+fig,ax = drawBaseStructure()
+drawLine(ax,lineAngle,allVertices[2],"sound wave")
+drawLine(ax,waveAngle,allVertices[0],"line perpendicular to sound wave",color="k",linewidth=0.5)
+drawLine(ax,waveAngle,(0,0),"angle of sound wave",color="green",linewidth=0.5, linestyle="dashed")
+drawAngle(ax,(0,0),0,waveAngle,0.7)
+ax.set_title(f'Wave at {waveAngle} Degrees hitting {hitOrder[0]}')
+drawAngle(ax,allVertices[0],waveAngle,210)
+drawAngle(ax,allVertices[0],210,270,0.15)
+plt.xlim(-0.5, 0.6)
+plt.ylim(-0.5, 0.6)
+plt.xlabel("X Axis Distance in meters (m)")
+plt.ylabel("Y Axis Distance in meters (m)")
+plt.legend()
+plt.show()
+
+fig,ax = drawBaseStructure()
+drawLine(ax,lineAngle,allVertices[0],"sound wave")
+drawLine(ax,waveAngle,allVertices[1],"line perpendicular to sound wave",color="k",linewidth=0.5)
+drawLine(ax,waveAngle,(0,0),"angle of sound wave",color="green",linewidth=0.5, linestyle="dashed")
+drawAngle(ax,(0,0),0,waveAngle,0.7)
+ax.set_title(f'Wave at {waveAngle} Degrees hitting {hitOrder[1]}')
+drawAngle(ax,allVertices[1],90,waveAngle,0.1)
+plt.xlim(-0.5, 0.6)
+plt.ylim(-0.5, 0.6)
+plt.xlabel("X Axis Distance in meters (m)")
+plt.ylabel("Y Axis Distance in meters (m)")
+plt.legend()
+plt.show()
