3016a2568f7fc0d34e14631285b22ab132c6e4c8
sayad.pervez <sayad.pervez@zohocorp.com>
2023-03-02 17:09:57 +0530

    all 6 completed

--- docgen/CBA.py
+++ docgen/CBA.py
@@ -23,9 +23,9 @@ def drawBaseStructure():
     # (1,0) & (0,2.732)
     return fig,ax
 
-hitOrder="CAB"
+hitOrder="CBA"
 
-waveAngle = 140 # degrees
+waveAngle = 220 # degrees
 lineAngle = waveAngle+90
 
 fig,ax = drawBaseStructure()
@@ -38,12 +38,12 @@ plt.show()
 
 fig,ax = drawBaseStructure()
 drawLine(ax,lineAngle,allVertices[2],"sound wave")
-drawLine(ax,waveAngle,allVertices[0],"line perpendicular to sound wave",color="k",linewidth=0.5)
+drawLine(ax,waveAngle,allVertices[1],"line perpendicular to sound wave",color="k",linewidth=0.5)
 drawLine(ax,waveAngle,(0,0),"angle of sound wave",color="green",linewidth=0.5, linestyle="dashed")
 drawAngle(ax,(0,0),0,waveAngle,0.7)
 ax.set_title(f'Wave at {waveAngle} Degrees hitting {hitOrder[0]}')
-drawAngle(ax,allVertices[0],waveAngle,210)
-drawAngle(ax,allVertices[0],210,270,0.15)
+drawAngle(ax,allVertices[1],150,waveAngle)
+drawAngle(ax,allVertices[1],90,150,0.15)
 plt.xlim(-0.5, 0.6)
 plt.ylim(-0.5, 0.6)
 plt.xlabel("X Axis Distance in meters (m)")
@@ -52,12 +52,12 @@ plt.legend()
 plt.show()
 
 fig,ax = drawBaseStructure()
-drawLine(ax,lineAngle,allVertices[0],"sound wave")
-drawLine(ax,waveAngle,allVertices[1],"line perpendicular to sound wave",color="k",linewidth=0.5)
+drawLine(ax,lineAngle,allVertices[1],"sound wave")
+drawLine(ax,waveAngle,allVertices[0],"line perpendicular to sound wave",color="k",linewidth=0.5)
 drawLine(ax,waveAngle,(0,0),"angle of sound wave",color="green",linewidth=0.5, linestyle="dashed")
 drawAngle(ax,(0,0),0,waveAngle,0.7)
 ax.set_title(f'Wave at {waveAngle} Degrees hitting {hitOrder[1]}')
-drawAngle(ax,allVertices[1],90,waveAngle,0.1)
+drawAngle(ax,allVertices[0],waveAngle,270,0.1)
 plt.xlim(-0.5, 0.6)
 plt.ylim(-0.5, 0.6)
 plt.xlabel("X Axis Distance in meters (m)")
