eb00e958cf0a9b96736a151aff7a98b3d36d30b2
sayad.pervez <sayad.pervez@zohocorp.com>
2023-03-02 17:29:21 +0530

    basicStructure functionized

--- docgen/_basicStructure.py
+++ docgen/_basicStructure.py
@@ -5,16 +5,21 @@ allVertices = getVertices()
 xLi = [_[0] for _ in allVertices]
 yLi = [_[1] for _ in allVertices]
 
-fig = plt.figure()
-ax = fig.add_subplot()
+def drawBaseStructure():
+    fig, ax = plt.subplots()
 
-plt.axhline(y=0, color='black', linestyle=':')
-plt.axvline(x=0, color='black', linestyle=':')
+    plt.axhline(y=0, color='black', linestyle=':')
+    plt.axvline(x=0, color='black', linestyle=':')
 
-plt.plot(xLi+[xLi[0]],yLi+[yLi[0]],c="b")
-plt.scatter(xLi,yLi,c="k")
+    plt.plot(xLi+[xLi[0]],yLi+[yLi[0]],c="b",label="Equilateral Triangular Structure")
+    plt.scatter(xLi,yLi,c="k",label="Vertices of Triangle")
 
-ax.set_aspect('equal', adjustable='box')
-ax.spines[['right', 'top']].set_visible(False)
+    plt.legend()
+
+    ax.set_aspect('equal', adjustable='box')
+    ax.spines[['right', 'top']].set_visible(False)
+    return fig,ax
+
+fig,ax = drawBaseStructure()
 
 plt.show()
