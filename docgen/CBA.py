2dd55f112aafcd01221f58448cd173b9371c4d77
sayad.pervez <sayad.pervez@zohocorp.com>
2023-03-02 17:31:36 +0530

    centralized basic structure

--- docgen/CBA.py
+++ docgen/CBA.py
@@ -1,24 +1,10 @@
 from matplotlib import pyplot as plt
 from formulae import getVertices,drawLine,drawAngle
+from _basicStructure import drawBaseStructure
 
 allVertices = getVertices()
 xLi = [_[0] for _ in allVertices]
 yLi = [_[1] for _ in allVertices]
-
-def drawBaseStructure():
-    fig, ax = plt.subplots()
-
-    plt.axhline(y=0, color='black', linestyle=':')
-    plt.axvline(x=0, color='black', linestyle=':')
-
-    plt.plot(xLi+[xLi[0]],yLi+[yLi[0]],c="b",label="Equilateral Triangular Structure")
-    plt.scatter(xLi,yLi,c="k",label="Vertices of Triangle")
-
-    plt.legend()
-
-    ax.set_aspect('equal', adjustable='box')
-    ax.spines[['right', 'top']].set_visible(False)
-    return fig,ax
 
 hitOrder="CBA"
 
