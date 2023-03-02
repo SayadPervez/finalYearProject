b069843683166763dec7d6803b311ea782180403
sayad.pervez <sayad.pervez@zohocorp.com>
2023-03-02 17:58:03 +0530

    ABC labels completed

--- docgen/_basicStructure.py
+++ docgen/_basicStructure.py
@@ -14,6 +14,9 @@ def drawBaseStructure():
     plt.plot(xLi+[xLi[0]],yLi+[yLi[0]],c="gray",linewidth=0.5)
     plt.scatter(xLi,yLi,c="k",label="Vertices of Equilateral Triangle")
 
+    ax.text(xLi[0]+0.01,yLi[0]+0.01,"A")
+    ax.text(xLi[1]+0.01,yLi[1]+0.01,"B")
+    ax.text(xLi[2]+0.01,yLi[2]+0.01,"C")
     plt.legend()
 
     ax.set_aspect('equal', adjustable='box')
